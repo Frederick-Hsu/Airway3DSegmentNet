03a49782f4c3090e4211e4d245f58141d741c946
Frederique Hsu <frederique.hsu@outlook.com>
2023-01-25 15:37:23 +0800

    修改utils.py的代码，增加如下：
    1. 函数 def load_CT_scan_3D_image(niigz_file_name)
    2. 类Logger
    3. 修改两个函数名 save_pickle,  load_pickle

--- utils.py
+++ utils.py
@@ -8,21 +8,43 @@
 
 
 import pickle
+import sys
+import SimpleITK as sitk
+import numpy as np
 
 
 # Functions ========================================================================================
-def save_obj(obj, filename):
-    with open(filename + ".pkl", "wb") as fh:
-        pickle.dump(obj, fh, pickle.HIGHEST_PROTOCOL)
+def save_pickle(data_dict, filename):
+    with open(filename, "wb") as fh:
+        pickle.dump(data_dict, fh, pickle.HIGHEST_PROTOCOL)
 
 
-def load_obj(filename):
-    with open(filename + ".pkl", "rb") as fh:
+def load_pickle(filename):
+    with open(filename, "rb") as fh:
         return pickle.load(fh)
 
 
-# Classes ==========================================================================================
+def load_CT_scan_3D_image(niigz_file_name):
+    itkimage = sitk.ReadImage(niigz_file_name)
+    numpyImages = sitk.GetArrayFromImage(itkimage)
+    numpyOrigin = np.array(list(reversed(itkimage.GetOrigin())))
+    numpySpacing = np.array(list(reversed(itkimage.GetSpacing())))
+    return numpyImages, numpyOrigin, numpySpacing
 
+
+# Classes ==========================================================================================
+class Logger:
+    def __init__(self, logfile):
+        self.terminal = sys.stdout
+        self.log = open(logfile, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        self.terminal.flush()
+        self.log.flush()
 
 
 # Main logics ======================================================================================
