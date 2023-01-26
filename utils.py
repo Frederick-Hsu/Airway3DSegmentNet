7d2875d306f6efa4c23405eba5e74201f8c87146
Frederique Hsu <frederique.hsu@outlook.com>
2023-01-26 20:26:06 +0800

    增加多个函数 dice_coefficient_np(), positive_predictive_value_np(), sensitivity_np(), accuracy_np(), 用于计算一些指标值。

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ import sys
 import SimpleITK as sitk
 import numpy as np
 
+smooth = 1.0
 
 # Functions ========================================================================================
 def save_pickle(data_dict, filename):
@@ -30,6 +31,44 @@ def load_CT_scan_3D_image(niigz_file_name):
     numpyOrigin = np.array(list(reversed(itkimage.GetOrigin())))
     numpySpacing = np.array(list(reversed(itkimage.GetSpacing())))
     return numpyImages, numpyOrigin, numpySpacing
+
+def dice_coefficient_np(predict_data, groundtruth_data):
+    r'''
+    Calculate the dice coefficient in the numpy type.
+
+    :param predict_data:
+    :param groundtruth_data:
+    both the 2 parameters must be numpy type
+
+    :return the dice coefficient value, in the numpy type
+    '''
+    groundtruth_data_flatten = groundtruth_data.flatten()
+    predict_data_flatten = predict_data.flatten()
+    intersection = np.sum(predict_data_flatten * groundtruth_data_flatten)
+    return 2.0 * (intersection + smooth) / (np.sum(predict_data_flatten) +
+                                            np.sum(groundtruth_data_flatten) +
+                                            smooth)
+
+
+def positive_predictive_value_np(predict_data, groundtruth_data):
+    predict_data_flatten = predict_data.flatten()
+    groundtruth_data_flatten = groundtruth_data.flatten()
+    intersection = np.sum(predict_data_flatten * groundtruth_data_flatten)
+    return (intersection + smooth) / (np.sum(predict_data_flatten) + smooth)
+
+
+def sensitivity_np(predict, groundtruth):
+    predict_flatten = predict.flatten()
+    groundtruth_flatten = groundtruth.flatten()
+    intersection = np.sum(predict_flatten * groundtruth_flatten)
+    return (intersection + smooth) / (np.sum(groundtruth_flatten) + smooth)
+
+
+def accuracy_np(predict, groundtruth):
+    predict_flatten = predict.flatten()
+    groundtruth_flatten = groundtruth.flatten()
+    intersection = np.sum(predict_flatten == groundtruth_flatten)
+    return intersection / (len(groundtruth_flatten) + smooth)
 
 
 # Classes ==========================================================================================
