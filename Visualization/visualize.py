bbe4a605a4794ee5ca21f5aae63b63ddd9c10227
Frederique Hsu <frederique.hsu@outlook.com>
2023-02-07 19:01:42 +0800

    1. Insert a new argument "phase" for both visualize_airway_tree_segment_effect() and visualize_bronchus_segment_slices()
    2. Do not specify the figsize, just utilize on the default figsize. Thus we can get the uniform figure size 640 x 480 in pixel.
    
    3. it is need to plt.close(fig) after you plt.savefig(), otherwise it will confuse previous figure and current figure.

--- Visualization/visualize.py
+++ Visualization/visualize.py
@@ -16,13 +16,13 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Functions ========================================================================================
 def visualize_airway_tree_segment_effect(epoch_num,
+                                         phase,
                                          case_name,
                                          raw_image_cuboid,
                                          label_cuboid,
                                          segment_cuboid,
                                          save_dir,
-                                         tensorboard_writer,
-                                         total_count):
+                                         tensorboard_writer):
     assert (raw_image_cuboid.shape == label_cuboid.shape == segment_cuboid.shape), \
         "The 3 cuboids must have the same shape."
 
@@ -37,38 +37,38 @@ def visualize_airway_tree_segment_effect(epoch_num,
     label_cuboid_front_view = np.flipud(label_cuboid_front_view)
     raw_image_cuboid_front_view = np.flipud(raw_image_cuboid_front_view)
 
-    fig = plt.figure(figsize=(20, 20))
+    fig = plt.figure()
     plt.title("{0}: airway tree segmentation at epoch{1}".format(case_name, epoch_num))
     plt.xlabel("cuboid width")
     plt.ylabel("cuboid depth")
     plt.imshow(raw_image_cuboid_front_view, cmap='gray')
-    plt.contour(label_cuboid_front_view, colors='r')
-    plt.contour(segment_cuboid_front_view, colors='g')
+    plt.contour(label_cuboid_front_view, colors='g')    # green marks the ground-truth, green <---> ground-truth
+    plt.contour(segment_cuboid_front_view, colors='r')  # red marks the segmentation
     fig_file = os.path.join(save_dir,
-                            "{0}_airway_tree_segmentation_at_epoch{1}.png".format(case_name, epoch_num))
+                            "{0}_airway_tree_segmentation_at_{1}_epoch{2}.png".format(case_name, phase, epoch_num))
     plt.savefig(fig_file)
+    plt.close(fig)
     # Read the figure file to convert it to numpy.array format
     img = plt.imread(fig_file)
 
-    total_count += 1
-    tensorboard_writer.add_image(tag="{0}: airway tree segmentation at epoch{1}".format(case_name, epoch_num),
+    tensorboard_writer.add_image(tag="{0}: airway tree segmentation at {1} epoch{2}".format(case_name, phase, epoch_num),
                                  img_tensor=img,
-                                 global_step=total_count,
+                                 global_step=epoch_num,
                                  dataformats="HWC")
     tensorboard_writer.flush()
-    plt.close(fig)
 
 
 def visualize_bronchus_segment_slices(epoch_num,
+                                      phase,
                                       case_name,
                                       raw_image_cuboid,
                                       label_cuboid,
                                       segment_cuboid,
                                       save_dir,
-                                      tensorboard_writer,
-                                      total_count):
+                                      tensorboard_writer):
     depth, height, width = label_cuboid.shape
 
+    total_count = 0
     # Extract 5 slices equally from the raw_image_cuboid
     for index in range(1, 6):
         slice_index = depth * index // 6
@@ -77,22 +77,22 @@ def visualize_bronchus_segment_slices(epoch_num,
         segment_slice = segment_cuboid[slice_index, :, :]
         assert len(image_slice.shape) == 2 and (image_slice.shape == label_slice.shape == segment_slice.shape)
 
-        fig = plt.figure(figsize=(20, 20))
+        fig = plt.figure()
         plt.title("{0}: bronchus slice #{1}, overlapping label and segment".format(case_name, slice_index))
         plt.xlabel("cuboid width")
         plt.ylabel("cuboid height")
         plt.imshow(image_slice, cmap='gray')
-        plt.contour(segment_slice, colors='r')
-        plt.contour(label_slice, colors='g')
+        plt.contour(segment_slice, colors='r')  # red marks the segmentation
+        plt.contour(label_slice, colors='g')    # green marks the ground-truth, green <---> ground-truth
 
         fig_file = os.path.join(save_dir,
-            "{0}_bronchus_segmentation_slice{1}_at_epoch{2}.png".format(case_name, slice_index, epoch_num))
+            "{0}_bronchus_segmentation_slice{1}_at_{2}_epoch{3}.png".format(case_name, slice_index, phase, epoch_num))
         plt.savefig(fig_file)
         plt.close(fig)
 
         img = plt.imread(fig_file)
         total_count += 1
-        tensorboard_writer.add_image(tag="{0}: bronchus segmentation at epoch{1}".format(case_name, epoch_num),
+        tensorboard_writer.add_image(tag="{0}: bronchus segmentation at {1} epoch{2}".format(case_name, phase, epoch_num),
                                      img_tensor=img,
                                      global_step=total_count,
                                      dataformats="HWC")
@@ -109,23 +109,22 @@ if __name__ == "__main__":
     segment_cuboid_npy = np.load(segment_cuboid_npy_file)
     raw_image_cuboid_npy = np.load(raw_image_cuboid_npy_file)
 
-    total_count = 1
     tensorboard_writer = SummaryWriter(log_dir="tensorboard")
     visualize_airway_tree_segment_effect(case_name='ATM_015_0000',
                                          raw_image_cuboid=raw_image_cuboid_npy,
                                          label_cuboid=label_cuboid_npy,
                                          segment_cuboid=segment_cuboid_npy,
                                          epoch_num=5,
+                                         phase='validate',
                                          save_dir="./",
-                                         tensorboard_writer=tensorboard_writer,
-                                         total_count=total_count)
+                                         tensorboard_writer=tensorboard_writer)
 
     visualize_bronchus_segment_slices(epoch_num=5,
+                                      phase='train',
                                       case_name='ATM_015_0000',
                                       raw_image_cuboid=raw_image_cuboid_npy,
                                       label_cuboid=label_cuboid_npy,
                                       segment_cuboid=segment_cuboid_npy,
                                       save_dir='./',
-                                      tensorboard_writer=tensorboard_writer,
-                                      total_count=total_count)
+                                      tensorboard_writer=tensorboard_writer)
     tensorboard_writer.close()
