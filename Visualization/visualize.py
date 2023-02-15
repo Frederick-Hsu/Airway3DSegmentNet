28aaac5a94bd6a4aedd819b702e12607dfa60a32
Frederique Hsu <frederique.hsu@outlook.com>
2023-02-15 08:02:15 +0800

    Revoke to save the airway tree plain figure, change to save the airway tree 3D model, annotated with 3colors or 2colors respectively.
    Meanwhile save the metrics of False-Positive-Rate, True-Positive-Rate, False-Negative-Rate and DSC.

--- Visualization/visualize.py
+++ Visualization/visualize.py
@@ -12,50 +12,52 @@ from matplotlib import cm
 import numpy as np
 import os
 from torch.utils.tensorboard import SummaryWriter
+from metrics_calculation import save_airway_tree_3dmodel_annotated_with_3colors, \
+                                save_airway_tree_3dmodel_annotated_with_2colors
 
 
 # Functions ========================================================================================
 def visualize_airway_tree_segment_effect(epoch_num,
                                          phase,
                                          case_name,
-                                         raw_image_cuboid,
                                          label_cuboid,
                                          segment_cuboid,
+                                         origin,
+                                         spacing,
                                          save_dir,
                                          tensorboard_writer):
-    assert (raw_image_cuboid.shape == label_cuboid.shape == segment_cuboid.shape), \
-        "The 3 cuboids must have the same shape."
+    assert (label_cuboid.shape == segment_cuboid.shape), "The 3 cuboids must have the same shape."
 
-    depth, height, width = label_cuboid.shape
+    niigz_file_name = "{0}/{1}_airway_tree_with_3colors_at_{2}_epoch{3}.nii.gz".format(save_dir,
+                                                                                       case_name,
+                                                                                       phase,
+                                                                                       epoch_num)
+    metrics = save_airway_tree_3dmodel_annotated_with_3colors(label_cuboid,
+                                                              segment_cuboid,
+                                                              origin,
+                                                              spacing,
+                                                              niigz_file_name)
+    FPR, TPR, FNR, DSC = metrics
+    tensorboard_writer.add_scalar(tag="{0}: False Positive Rate at {1} phase".format(case_name, phase),
+                                  scalar_value=FPR,
+                                  global_step=epoch_num)
+    tensorboard_writer.add_scalar(tag="{0}: True Positive Rate at {1} phase".format(case_name, phase),
+                                  scalar_value=TPR,
+                                  global_step=epoch_num)
+    tensorboard_writer.add_scalar(tag="{0}: False Negative Rate at {1} phase".format(case_name, phase),
+                                  scalar_value=FPR,
+                                  global_step=epoch_num)
+    tensorboard_writer.add_scalar(tag="{0}: DSC at {1} phase".format(case_name, phase),
+                                  scalar_value=DSC,
+                                  global_step=epoch_num)
 
-    segment_cuboid_front_view = np.sum(segment_cuboid, axis=1)
-    label_cuboid_front_view = np.sum(label_cuboid, axis=1)
-    raw_image_cuboid_front_view = raw_image_cuboid[:, height * 3 // 4, :]
+    niigz_file_name = "{0}/{1}_airway_tree_with_2colors_at_{2}_epoch{3}.nii.gz".format(save_dir,
+                                                                                       case_name,
+                                                                                       phase,
+                                                                                       epoch_num)
+    save_airway_tree_3dmodel_annotated_with_2colors(label_cuboid, segment_cuboid, origin, spacing, niigz_file_name)
 
-    # Flip Up/Down the cuboid
-    segment_cuboid_front_view = np.flipud(segment_cuboid_front_view)
-    label_cuboid_front_view = np.flipud(label_cuboid_front_view)
-    raw_image_cuboid_front_view = np.flipud(raw_image_cuboid_front_view)
 
-    fig = plt.figure()
-    plt.title("{0}: airway tree segmentation at epoch{1}".format(case_name, epoch_num))
-    plt.xlabel("cuboid width")
-    plt.ylabel("cuboid depth")
-    plt.imshow(raw_image_cuboid_front_view, cmap='gray')
-    plt.contour(label_cuboid_front_view, colors='g')    # green marks the ground-truth, green <---> ground-truth
-    plt.contour(segment_cuboid_front_view, colors='r')  # red marks the segmentation
-    fig_file = os.path.join(save_dir,
-                            "{0}_airway_tree_segmentation_at_{1}_epoch{2}.png".format(case_name, phase, epoch_num))
-    plt.savefig(fig_file)
-    plt.close(fig)
-    # Read the figure file to convert it to numpy.array format
-    img = plt.imread(fig_file)
-
-    tensorboard_writer.add_image(tag="{0}: airway tree segmentation at {1} epoch{2}".format(case_name, phase, epoch_num),
-                                 img_tensor=img,
-                                 global_step=epoch_num,
-                                 dataformats="HWC")
-    tensorboard_writer.flush()
 
 
 def visualize_bronchus_segment_slices(epoch_num,
